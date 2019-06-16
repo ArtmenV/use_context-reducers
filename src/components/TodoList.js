fc4c1472b94a52ef5969601c39e1ed17da5e043b
Artmen_V <viliutich.artem@gmail.com>
2019-06-16 16:15:44 +0700

    add_style

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,18 @@ import TodosContext from '../context'
 
 export default function TodoList() {
     const { state } = useContext(TodosContext)
+    const title = state.todos.length > 0 
+        ? `${state.todos.length} Todos` 
+        :   "Nothing To Do!"
+
 
     return (
-        <div>
-            <ul>
+        <div className = "container mx-auto max-w-md text-center font-mono" >
+           <h1 className="text-bold"><strong> {title}</strong> </h1>
+            <ul className="list-reset text-white p0">
                 {state.todos.map(todo => (
-                    <li key={todo.id}>
+                    <li key={todo.id}
+                        className=" bg-orane-dark orange border-black border-dashed border-2">
                        <span> {todo.text} </span>
                         </li>
                 ))}
